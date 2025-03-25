562a2a218f748caa4dac62e5270a729de1c08294
EmaDev <pavanello.emanuele@gmail.com>
2025-03-25 14:08:18 +0100

    qta

--- src/routes/api/stripe-create-session/+server.ts
+++ src/routes/api/stripe-create-session/+server.ts
@@ -17,10 +17,10 @@ export const POST: RequestHandler = async ({ locals: { session } }) => {
       line_items: [
         {
           price: PUBLIC_STRIPE_PRICE_ID,
-          quantity: 25,
+          quantity: 15,
           adjustable_quantity: {
             enabled: true,
-            minimum: 25,
+            minimum: 15,
             maximum: 100
           }
         }
