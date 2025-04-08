9e3ac8bccca75f019c84761ba0619fa2cd48fe5c
EmaDev <pavanello.emanuele@gmail.com>
2025-04-08 12:32:50 +0200

    fix

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -2,8 +2,13 @@
   import { cn } from '$lib/utils';
 </script>
 
-<p class={cn('bg bottom-0 -mt-2 mb-2 w-full px-4 py-2 text-center text-xs', $$restProps.class)}>
-  <span class="rounded-md bg-base-100/50 px-2 py-1">
+<p
+  class={cn(
+    'bg pointer-events-none bottom-0 -mt-2 mb-2 w-full px-4 py-2 text-center text-xs',
+    $$restProps.class
+  )}
+>
+  <span class="pointer-events-auto rounded-md bg-base-100/50 px-2 py-1">
     Made with ❤️ by <a class="font-semibold underline" href="https://emadev.co" target="_blank"
       >EmaDev</a
     >
